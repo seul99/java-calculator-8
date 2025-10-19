284dbb42cb1573cdec18f33e39358c115f744844
sia <piao516@naver.com>
2025-10-19 18:58:24 +0900

    구분자를 기준으로 분리한 숫자의 합 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        // 코드 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
